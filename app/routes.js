928fca6c0af57db2213f1c385ae0f876e7579774
pavasnavaney <pavasnavani@gmail.com>
2019-12-03 08:02:13 -0800

    Added Support for Uploads

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,33 @@
 // app/routes.js
+
+var multer = require('multer');
+var crypto = require('crypto');
+var fs = require('fs');
+var mime = require('mime');
+var storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			var hash = crypto.createHash('sha256').update(JSON.stringify(req.user)).digest('base64');
+			hash = hash.replace('/','');
+			var newDestination = 'uploads/' + hash;
+			var stat = null;
+			try {
+            stat = fs.statSync(newDestination);
+        } catch (err) {
+            fs.mkdirSync(newDestination);
+        }
+        if (stat && !stat.isDirectory()) {
+            throw new Error('Directory cannot be created because an inode of a different type exists at "' + dest + '"');
+        }
+	    cb(null, newDestination);
+	  },
+		filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+    });
+  }
+});
+
+var upload = multer({ storage: storage })
 module.exports = function(app, passport) {
 
 	app.get('/', function(req, res) {
@@ -9,10 +38,21 @@ module.exports = function(app, passport) {
 		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
 
+	app.post('/upload', isLoggedIn , upload.single('pic'), (req, res, next) => {
+  const file = req.file
+  if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }
+    res.send(file)
+
+})
+
 	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/profile', 
-            failureRedirect : '/login', 
-            failureFlash : true 
+            successRedirect : '/profile',
+            failureRedirect : '/login',
+            failureFlash : true
 		}),
         function(req, res) {
             console.log("hello");
@@ -29,14 +69,14 @@ module.exports = function(app, passport) {
 	});
 
 	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile', 
-		failureRedirect : '/signup', 
-		failureFlash : true 
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
 	}));
 
 	app.get('/profile', isLoggedIn, function(req, res) {
 		res.render('profile.ejs', {
-			user : req.user 
+			user : req.user
 		});
 	});
 	app.get('/logout', function(req, res) {
