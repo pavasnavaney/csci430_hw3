cd28262768b44676498caa0135d65d5e908f20ed
SachitNayak <sachitmnayak@gmail.com>
2019-12-04 17:23:35 -0800

    minor configuration changes.

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 module.exports = {
     'connection': {
         'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'user': 'webuser',
+        'password': 'XZ9E2dJQWyVKKw=='
     },
 	'database': 'csci430',
     	'users_table': 'users'
